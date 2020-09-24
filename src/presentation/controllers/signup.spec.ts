304e3cd8bb0d959f98d8fa9c2ef1667e5800f35e
João Trindade <joaotrindade.soft@gmail.com>
2020-09-24 12:40:38 +0100

    test: ensure SignUpController return 200 if all params are provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,3 +31,20 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
+
+describe('Signup Controller', () => {
+  test('Should return 200 if all params are provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toBe('Success')
+  })
+})
